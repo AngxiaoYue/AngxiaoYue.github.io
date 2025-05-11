3d61e70262db92be321012801853aeee075ff798
Angxiao Yue <yueangxiao@163.com>
2025-05-11 21:14:28 +0800

    update

--- assets/js/bibtex-handler.js
+++ assets/js/bibtex-handler.js
@@ -14,6 +14,22 @@ const bibtexData = {
   number={16},
   pages={16826--16834},
   year={2025}
+}`,
+  "QMP": `@inproceedings{yue2024plug,
+  title={A plug-and-play quaternion message-passing module for molecular conformation representation},
+  author={Yue, Angxiao and Luo, Dixin and Xu, Hongteng},
+  booktitle={Proceedings of the AAAI Conference on Artificial Intelligence},
+  volume={38},
+  number={15},
+  pages={16633--16641},
+  year={2024}
+}`,
+  "USFGW": `@inproceedings{luo2022weakly,
+  title={Weakly-supervised temporal action alignment driven by unbalanced spectral fused Gromov-Wasserstein distance},
+  author={Luo, Dixin and Wang, Yutong and Yue, Angxiao and Xu, Hongteng},
+  booktitle={Proceedings of the 30th ACM International Conference on Multimedia},
+  pages={728--739},
+  year={2022}
 }`
   // 您可以添加更多论文的BibTeX内容
 };
